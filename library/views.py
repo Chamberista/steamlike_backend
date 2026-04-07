76fa64f5c47a8dabbec7a20e05e4a5b87102c083
Chamberista <manuelroldanrodriguez2311@gmail.com>
2026-04-07 13:51:52 +0200

    Add entries endpoint and update URL routing

--- library/views.py
+++ library/views.py
@@ -1,6 +1,12 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
 @require_GET
 def health(request):
     return JsonResponse({"status": "ok"})
+@csrf_exempt
+@require_POST
+def entries(request):
+    return JsonResponse({"external_game_id(string)" + "status(whislist)" +"hours_played(0)"})
